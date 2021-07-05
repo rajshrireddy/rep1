756e9581c485c4998a4f9f01d47df5fe9d52a507
rajshrireddy <godalwarrajshri0805@gmail.com>
2021-07-05 17:44:58 +0530

    terraform

---
+++ instance.tf
@@ -0,0 +1,12 @@
+resource aws_instance "i1"{
+ami = "ami-09e67e426f25ce0d7"
+instance_type= "t2.micro"
+}
+resource aws_instance "i2"{
+ami = "ami-09e67e426f25ce0d7"
+instance_type= "t2.micro"
+}
+resource aws_instance "i3"{
+ami = "ami-09e67e426f25ce0d7"
+instance_type= "t2.micro"
+}
---
+++ prvd.tf
@@ -0,0 +1,5 @@
+provider "aws"{
+region="ap-south-1"
+access_key="AKIAVKE2SHVRGEU5QGGZ"
+secret_key="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"
+}
