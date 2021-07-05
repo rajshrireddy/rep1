02dce5938247d68609759a42e4891c2d37cd7d47
rajshrireddy <godalwarrajshri0805@gmail.com>
2021-07-05 19:25:26 +0530

    terraform

--- instance.tf
+++ instance.tf
@@ -1,12 +1,15 @@
 resource aws_instance "i1"{
 ami = "ami-0c1a7f89451184c8b"
 instance_type= "t2.micro"
+key_pair= "5july.pem"
 }
 resource aws_instance "i2"{
 ami = "ami-0c1a7f89451184c8b"
 instance_type= "t2.micro"
+key_pair= "5july.pem"
 }
 resource aws_instance "i3"{
 ami = "ami-0c1a7f89451184c8b"
 instance_type= "t2.micro"
+key_pair= "5july.pem"
 }
