1cd991a82cd364e29f3235fbf61f3af6e611973a
rajshrireddy <godalwarrajshri0805@gmail.com>
2021-07-05 17:50:35 +0530

    terraform

--- prvd.tf
+++ prvd.tf
@@ -1,5 +1,5 @@
 provider "aws"{
 region="ap-south-1"
-access_key="AKIAVKE2SHVRGEU5QGGZ"
-secret_key="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"
+access_key="AKIAVKE2SHVRE2F7QQ4D"
+secret_key="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"
 }
