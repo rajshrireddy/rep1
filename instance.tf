ab9777106fb4408502bc6302d3ddf40fbfb808e5
rajshrireddy <godalwarrajshri0805@gmail.com>
2021-07-06 16:54:56 +0530

    terraform

--- instance.tf
+++ instance.tf
@@ -2,7 +2,7 @@ resource aws_instance "i1"{
 count=3
 ami = "ami-09e67e426f25ce0d7"
 instance_type= "t2.micro"
-key_name= "6july.pem"
+key_name= "july6"
  provisioner "local-exec" {
     command = "echo ${self.private_ip} >> private_ips.txt"
   }
